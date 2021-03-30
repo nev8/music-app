d54aebe91dafedd4bfe6b707329f1f5fe0cf18eb
Nevena <nevenablagojevic95@gmail.com>
2021-03-30 12:44:59 +0200

    nav and footer html

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title></title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css"
+        integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+    <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="scss/style.css">
+</head>
+
+<body>
+    <!-- Nav -->
+    <nav>
+        <a href="index.php" class="logo"><span>Music</span>App</a>
+        <ul>
+            <li><a href="index.php"></a>Home</li>
+            <li><a href="how-it-works"></a>How it works</li>
+            <li><a href="subscription"></a>Subscription</li>
+        </ul>
+    </nav>
+
+    <!--Footer-->
+    <footer>
+        <a href="index.php" class="logo"><span>Music</span>App</a>
+        <p>&copy; 2021. All right reserved</p>
+        <div class="icons">
+        <a href=""><i class="fab fa-facebook-square"></i></a>
+        <a href=""><i class="fab fa-instagram"></i></a>
+        <a href=""><i class="fab fa-soundcloud"></i></a>
+        <a href=""><i class="fab fa-youtube"></i></a>
+
+        </div>
+    </footer>
+
+
+</body>
+
+</html>
