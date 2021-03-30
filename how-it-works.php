f8239e59f7b77429ceb8066a4db67838a0fc30c0
Nevena <nevenablagojevic95@gmail.com>
2021-03-30 20:57:06 +0200

    hiwHeader  html

--- how-it-works.php
+++ how-it-works.php
@@ -1,7 +1,28 @@
 <?php include "partials/header.php"; ?>
 
+<!--HiwHeader-->
+<section class="hiwHeader">
+    <h2 class="container">How it works</h2>
+    <article class="container">
+        <div>
+            <i class="fas fa-user-circle"></i>
+            <h3>Create an accont</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita earum eum aspernatur autem ut odit ullam, repudiandae consequuntur nesciunt aliquid.</p>
 
-
-    <h1 style="font-size: 9rem; text-align: center;">How it works</h1>
+        </div>
+        <div>
+            <i class="fas fa-drum"></i>
+            <h3>Create an accont</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita earum eum aspernatur autem ut odit ullam, repudiandae consequuntur nesciunt aliquid.</p>
+            
+        </div>
+        <div>
+            <i class="fas fa-cloud-download-alt"></i>
+            <h3>Create an accont</h3>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Expedita earum eum aspernatur autem ut odit ullam, repudiandae consequuntur nesciunt aliquid.</p>
+            
+        </div>
+    </article>
+</section>
 
  <?php include "partials/footer.php"; ?>
