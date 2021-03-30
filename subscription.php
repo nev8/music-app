2ee874b791a5faf9ef81e4c031605b18e9842d18
Nevena <nevenablagojevic95@gmail.com>
2021-03-30 21:50:25 +0200

    concept html and css

--- subscription.php
+++ subscription.php
@@ -2,6 +2,31 @@
 
 
 
-    <h1 style="font-size: 9rem; text-align: center;">Subscription</h1>
+    <!--Artists-->
+ <section class="container concept">
+     <article>
+         <h2>Our Concept & artists</h2>
+         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.
+ Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.</p>
+     </article> 
+     <article>
+         <div>
+             <img src="img/subscription1.jpg" alt="">
+             <h3>No ad interruptions</h3>
+         </div>
+         <div>
+             <img src="img/subscription2.jpg" alt="">
+             <h3>High Quality</h3>
+         </div>
+         <div>
+             <img src="img/subscription3.jpg" alt="">
+             <h3>Listen Offline</h3>
+         </div>
+         <div>
+             <img src="img/subscription4.jpg" alt="">
+             <h3>Download music</h3>
+         </div>
+     </article>
+ </section>
 
  <?php include "partials/footer.php"; ?>
