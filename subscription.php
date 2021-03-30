2f67b4faf94298a8f01c0387e0140cc15ae7cc08
Nevena <nevenablagojevic95@gmail.com>
2021-03-30 23:53:04 +0200

    subscription html

--- subscription.php
+++ subscription.php
@@ -1,32 +1,56 @@
 <?php include "partials/header.php"; ?>
 
+<!--SubHeader-->
+<section class="subscription">
+    <article class="container">
+        <div>
+            <h2>Subscription from $15/month</h2>
+            <h4>Start a free trial now</h4>
+            <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Omnis similique iure nobis, facilis eaque, tempora exercitationem dolore esse animi quae delectus impedit perferendis ipsum, perspiciatis unde. Quae eligendi qui at.</p>
+            <button>Start free trail now</button>
+        </div>
+        <ul>
+        <li>Play any track</li>
+        <li>Listen offline</li>
+        <li>No add interruptions</li>
+        <li>Unlimited skips</li>
+        <li>High quality</li>
+        <li>Shuffle play</li>
+        </ul>
+        <div>
+        </div>
+    </article>
+</section>
 
+<!--Artists-->
+<section class="container concept">
+    <article>
+        <h2>Our Concept & artists</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi
+            officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto
+            sapiente laboriosam.
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem
+            nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto
+            sapiente laboriosam.</p>
+    </article>
+    <article>
+        <div>
+            <img src="img/subscription1.jpg" alt="">
+            <h3>No ad interruptions</h3>
+        </div>
+        <div>
+            <img src="img/subscription2.jpg" alt="">
+            <h3>High Quality</h3>
+        </div>
+        <div>
+            <img src="img/subscription3.jpg" alt="">
+            <h3>Listen Offline</h3>
+        </div>
+        <div>
+            <img src="img/subscription4.jpg" alt="">
+            <h3>Download music</h3>
+        </div>
+    </article>
+</section>
 
-    <!--Artists-->
- <section class="container concept">
-     <article>
-         <h2>Our Concept & artists</h2>
-         <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.
- Lorem ipsum dolor sit amet, consectetur adipisicing elit. Deleniti corporis velit excepturi ipsam quidem nisi officiis veniam, molestiae, commodi rem. Repudiandae id officiis repellat corporis eum accusamus iusto sapiente laboriosam.</p>
-     </article> 
-     <article>
-         <div>
-             <img src="img/subscription1.jpg" alt="">
-             <h3>No ad interruptions</h3>
-         </div>
-         <div>
-             <img src="img/subscription2.jpg" alt="">
-             <h3>High Quality</h3>
-         </div>
-         <div>
-             <img src="img/subscription3.jpg" alt="">
-             <h3>Listen Offline</h3>
-         </div>
-         <div>
-             <img src="img/subscription4.jpg" alt="">
-             <h3>Download music</h3>
-         </div>
-     </article>
- </section>
-
- <?php include "partials/footer.php"; ?>
+<?php include "partials/footer.php"; ?>
